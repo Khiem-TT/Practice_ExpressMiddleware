7f965ee3c3b14b207aabc3cb1beacf5b22e4b001
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-06-14 11:05:43 +0700

    second commit

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import helmet from "helmet";
 
 const port = 8000;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(morgan('common'));
+app.use(helmet());
 
 app.get('/', (req, res) => {
     res.json({
